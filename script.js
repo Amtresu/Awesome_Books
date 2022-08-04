2ed3709a3616d0aa40deeff38e93d38d04087ca9
Amtresu <conor.follette@gmail.com>
2022-08-05 01:05:37 +0900

    added final styling

--- script.js
+++ script.js
@@ -1,41 +1,115 @@
-class Book {
-  constructor(title, author) {
-    this.title = title
-    this.author = author
-    this.booksArray = []
-    this.BookData = document.querySelector('#books');
-    this.button = document.querySelector('#add-button');
-    this.titleInput = document.querySelector('#title');
-    this.authorInput = document.querySelector('#author');
-    this.sectionInput = document.querySelector('#books');
-    this.removeButton = document.querySelector('.remove');
-    this.ul = document.querySelector('.booksList')
-    this.message = document.querySelector('.message')
+const BookData = document.querySelector('#books');
+const button = document.querySelector('#add-button');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+
+class Storage {
+  constructor() {
+    this.Books = [];
   }
 
-  addBook() {
-    this.button.addEventListener('click', (e) => {
-      e.preventDefault();
-      const titleValue = this.title.value;
-      const authorValue = this.author.value;
-      this.booksArray.push({ titleValue, authorValue });
-      localStorage.setItem('book', JSON.stringify(this.booksArray));
-      this.sectionInput.innerHTML += `
-      <li class="book-item">
-      <p  class="book-title">"${titleValue}"</p>
-      <p>${authorValue}</p>
-      <button class="remove" onclick='remove(${titleValue})'>Remove</button>
-      </li> `;
-      this.message.innerHTML=`Book Added Succesfully`
+  static addBook(newBook) {
+    this.Books.push(newBook);
+    localStorage.setItem('Books', JSON.stringify(this.Books));
+  }
+
+  static removeFromBooks(target) {
+    const removeBook = target.previousElementSibling.firstElementChild.textContent;
+
+    this.Books.filter((book, index) => {
+      if (book.title === removeBook) {
+        this.Books.splice(index, 1);
+      }
+      return this.Books;
     });
+    localStorage.setItem('Books', JSON.stringify(this.Books));
   }
 
+  static getBooksFromStorage() {
+    if (localStorage.getItem('Books') === null) {
+      this.Books = [];
+    } else {
+      this.Books = JSON.parse(localStorage.getItem('Books'));
+    }
+    return this.Books;
+  }
 }
 
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const book = new Book(title, author);
-book.addBook();
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function UI() {}
+
+UI.prototype.addBookToUI = function (newBook) {
+  Storage.Books.forEach((book, index) => {
+    if (book.title === newBook.title) {
+      if (index % 2 === 0) {
+        BookData.innerHTML += `
+          <li class='book book-item'>
+          <div class ='info'>
+            <p class='book-title'>${newBook.title}</p>
+            <p class='book-author'>&nbsp by ${newBook.author}</p>
+          </div>
+            <button class='remove remove-btn btn' type='button'>Remove</button>
+          </li>
+        `;
+      } else {
+        BookData.innerHTML += `
+          <li class='book book-item'>
+          <div class ='info'>
+            <p class='book-title'>${newBook.title}</p>
+            <p class='book-author'>&nbsp by ${newBook.author}</p>
+          </div>
+            <button class='remove remove-btn btn' type='button'>Remove</button>
+          </li>
+        `;
+      }
+    }
+  });
+};
+
+UI.prototype.clearInputs = function (element1, element2) {
+  element1.value = '';
+  element2.value = '';
+};
+
+UI.prototype.removeBookFromUI = function (target) {
+  target.parentElement.remove();
+};
+
+const ui = new UI();
+
+function addBook(e) {
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  const newBook = new Book(title, author);
+
+  Storage.addBook(newBook);
+
+  ui.addBookToUI(newBook);
+  ui.clearInputs(titleInput, authorInput);
+
+  e.preventDefault();
+}
+
+function removeBook(e) {
+  if (e.target.className === 'remove remove-btn btn') {
+    ui.removeBookFromUI(e.target);
+    Storage.removeFromBooks(e.target);
+  }
+}
+
+button.addEventListener('click', addBook);
+BookData.addEventListener('click', removeBook);
+document.addEventListener('DOMContentLoaded', () => {
+  const allBooks = Storage.getBooksFromStorage();
+  allBooks.forEach((book) => ui.addBookToUI(book));
+});
+
+
 
 const addNew = document.querySelector('.add-new')
 const form = document.querySelector('.form')
@@ -49,21 +123,21 @@ const contactDiv = document.querySelector('.contact-info-div')
 
 
 addNew.addEventListener('click', () => {
-  form.style.visibility = 'visible'
+  form.style.display = 'unset'
   listDiv.style.display = 'none'
-  contactDiv.style.visibility = 'hidden'
+  contactDiv.style.display = 'none'
 })
 
 list.addEventListener('click', () => {
-  form.style.visibility = 'hidden'
+  form.style.display = 'none'
   listDiv.style.display = 'unset'
-  contactDiv.style.visibility = 'hidden'
+  contactDiv.style.display = 'none'
 })
 
 contact.addEventListener('click', () => {
-  form.style.visibility = 'hidden'
+  form.style.display = 'none'
   listDiv.style.display = 'none'
-  contactDiv.style.visibility = 'visible'
+  contactDiv.style.display = 'unset'
 })
 
 
